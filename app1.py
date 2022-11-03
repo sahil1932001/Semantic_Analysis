166dae5322c555251ad5066c8a0843f0e7463e1c
sahil shaikh <95639758+sahil1932001@users.noreply.github.com>
2022-11-03 19:06:00 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -14,17 +14,17 @@ st.image("Semantic Analysis.jpg",width = 800)
 st.markdown("#### Semantic Similarity, or Semantic Textual Similarity, is a task in the area of Natural Language Processing (NLP) that scores the relationship between texts or documents using a defined metric.There have been a lot of approaches for Semantic Similarity. The most straightforward and effective method now is to use a powerful model (e.g. transformer) to encode sentences to get their embeddings and then use a similarity metric (e.g. cosine similarity) to compute their similarity score.")
 
 
-sentence_1 = st.text_input(label = 'enter the 1st sentence ', label_visibility="visible")
-
-sentence_2 = st.text_input(label = 'enter the 2nd sentence ', label_visibility="visible")
-
-
 from sentence_transformers import  util
 
 #model = SentenceTransformer('stsb-mpnet-base-v2')
 
 pickle_in = open("model.pkl","rb")
 model_new = pickle.load(pickle_in)
+
+
+sentence_1 = st.text_input(label = 'enter the 1st sentence ', label_visibility="visible")
+
+sentence_2 = st.text_input(label = 'enter the 2nd sentence ', label_visibility="visible")
 
 # encode sentences to get their embeddings
 embedding1 = model_new.encode(sentence_1, convert_to_tensor=True)
